f2237a16663a9bef45bb0beb0a2703bb79680e36
socialclimber <newtnnkeng@gmail.com>
2020-10-20 03:21:47 +0200

    first TypeScript code

---
+++ arrow.js
@@ -0,0 +1,9 @@
+// define a function and assign it to a variable
+var log1 = function (message) {
+    console.log(message);
+};
+// alternatively, we can use tharrow function
+var log2 = function (message) {
+    console.log(message);
+};
+log2("Hi there");
---
+++ arrow.ts
@@ -0,0 +1,11 @@
+// define a function and assign it to a variable
+let log1 = function(message){
+    console.log(message);
+}
+
+// alternatively, we can use tharrow function
+let log2 = (message) => {
+    console.log(message);
+}
+
+log2("Hi there");
